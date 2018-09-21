3a73ea81013d2cc0759ea23f16068a957fca509a
agdwm <almudenaguerraswm@gmail.com>
2018-09-21 10:03:30 +0200

    Iteration 1 done - Sign Up

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,23 +1,25 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
 
 
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/uber-for-loundry', {useMongoClient: true})
-  .then(() => {
-    console.log('Connected to Mongo!')
-  }).catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+	.connect('mongodb://localhost/uber-for-loundry', {
+		useMongoClient: true
+	})
+	.then(() => {
+		console.log('Connected to Mongo!')
+	}).catch(err => {
+		console.error('Error connecting to mongo', err)
+	});
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -27,17 +29,19 @@ const app = express();
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 app.use(cookieParser());
 
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
+	src: path.join(__dirname, 'public'),
+	dest: path.join(__dirname, 'public'),
+	sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -52,7 +56,10 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 const index = require('./routes/index');
+const authRoutes = require('./routes/auth');
+
 app.use('/', index);
+app.use('/', authRoutes);
 
 
 module.exports = app;
